6b350aaf1a68829b21a3cfb0bb61fabf1bc1ca14
Valeria <Koroleva.Valeria.97@yandex.ru>
2023-08-25 18:58:05 +0300

    валидация телефона

--- src/script/components/validateInputs.js
+++ src/script/components/validateInputs.js
@@ -17,7 +17,8 @@ const validDataInputs = {
         errorMessage:'Введите правильный email',
     },
     phone: {
-        RegExp: /^\+?\d{10,15}$/,
+        // RegExp: /^\+?\d{10,15}$/,
+        RegExp: /(\+7|8)[\s(]*\d{3}[)\s]*\d{3}[\s-]?\d{2}[\s-]?/,
         errorMessage:'Телефон от 10 до 15 символов, может начинается с плюса'
     },
     inn: {
@@ -41,7 +42,6 @@ function checkValid(input, data) {
     const isValid = nameInput.RegExp.test(input.value);
     if (!isValid) {
         error.textContent = nameInput.errorMessage;
-        // error.style.display = 'block';
         input.style.borderBottomColor = "var(--Orange)";
         input.style.color = "var(--Orange)";
     } else {
@@ -53,9 +53,48 @@ function checkValid(input, data) {
         }
     }
 }
+
 //сбор данных с формы
 function collectData(data,formInputs) {
     if (Object.keys(data).length === formInputs.length) {
         console.log(data);
     }
 }
+
+
+window.addEventListener("DOMContentLoaded", function() {
+    [].forEach.call( document.querySelectorAll('.recipient-input4'), function(input) {
+        var keyCode;
+        function mask(event) {
+            event.keyCode && (keyCode = event.keyCode);
+            var pos = this.selectionStart;
+            if (pos < 3) event.preventDefault();
+            var matrix = "+7 ___ ___ ____",
+                i = 0,
+                def = matrix.replace(/\D/g, ""),
+                val = this.value.replace(/\D/g, ""),
+                new_value = matrix.replace(/[_\d]/g, function(a) {
+                    return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+                });
+            i = new_value.indexOf("_");
+            if (i != -1) {
+                i < 5 && (i = 3);
+                new_value = new_value.slice(0, i)
+            }
+            var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+                function(a) {
+                    return "\\d{1," + a.length + "}"
+                }).replace(/[+()]/g, "\\$&");
+            reg = new RegExp("^" + reg + "$");
+            if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+            if (event.type == "blur" && this.value.length < 5)  this.value = ""
+        }
+
+        input.addEventListener("input", mask, false);
+        input.addEventListener("focus", mask, false);
+        input.addEventListener("blur", mask, false);
+        input.addEventListener("keydown", mask, false)
+
+    });
+
+});
