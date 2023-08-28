5f810d44684b1c92b238b7057128cab37b61bdd7
Valeria <Koroleva.Valeria.97@yandex.ru>
2023-08-28 18:19:04 +0300

    коррекция

--- src/script/script.js
+++ src/script/script.js
@@ -1,5 +1,4 @@
 import './components/render.js'
-import "./components/changeBasketItems.js"
-import "./components/chengeBasketOrder.js"
+import "./components/chengeBasket.js"
 import "./components/validateInputs.js"
 import "./components/modal.js"
